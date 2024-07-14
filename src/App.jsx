d2f742b17fc8d3553eea0895bf4fe793378b9a9c
kiran-jawale <kiran464jawale@gmail.com>
2024-07-14 16:25:57 +0530

    Day 1 of React revision

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Day1 from "./components/Day1";
+
+const App = () => {
+  return (
+    <>
+      <Day1 />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Day1.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Day1 = () => {
+  const [state, setState] = useState([
+    "SUNDAY",
+    "MONDAY",
+    "TUESDAY",
+    "WENESDAY",
+    "THURSDAY",
+    "FRIDAY",
+    "SATURDAY",
+  ]);
+
+  const removeDay = (day) => {
+    setState((prev) => state.filter( (item) => item !== day ))
+  }
+
+  return (
+    <div className="bg-zinc-800 h-screen w-screen text-white font-semibold font-sans">
+      {state.map((day, index) => (
+        <li key={index}>{day} <button onClick={() => removeDay(day)}>Delete</button> </li>
+      ))}
+    </div>
+  );
+};
+
+export default Day1;
